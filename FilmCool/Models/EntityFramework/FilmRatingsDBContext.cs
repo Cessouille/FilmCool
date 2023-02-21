bf2b5c9f8a0a510253b0ae27c9cabb8ad3e3fe5d
Cessouille <celian.chausson@etu.univ-smb.fr>
2023-02-21 11:36:13 +0100

    Fin td lundi

---
+++ FilmCool/Models/EntityFramework/FilmRatingsDBContext.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Runtime.Intrinsics.X86;
+
+namespace FilmCool.Models.EntityFramework
+{
+    public partial class FilmRatingsDBContext : DbContext
+    {
+        public FilmRatingsDBContext()
+        {
+        }
+
+        public FilmRatingsDBContext(DbContextOptions<FilmRatingsDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Notation> Notations { get; set; }
+
+        public virtual DbSet<Utilisateur> Utilisateurs { get; set; }
+
+        public virtual DbSet<Film> Films { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+            => optionsBuilder.UseNpgsql("Server=localhost;port=5432;Database=FilmsDB; uid=postgres; password=postgres;");
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Notation>(entity =>
+            {
+                entity.HasKey(e => new { e.FilmId, e.UtilisateurId })
+                    .HasName("pk_notation");
+
+                entity.HasOne(d => d.FilmNote).WithMany(p => p.NotesFilm)
+                    .HasForeignKey(d => d.FilmId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("fk_not_flm");
+
+                entity.HasOne(d => d.UtilisateurNotant).WithMany(p => p.NotesUtilisateur)
+                    .HasForeignKey(d => d.UtilisateurId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("fk_not_utl");
+            });
+
+            modelBuilder.Entity<Utilisateur>(entity =>
+            {
+                entity.Property(b => b.Pays).HasDefaultValue("France");
+
+                entity.Property(b => b.DateCreation).HasDefaultValueSql("now()");
+
+                entity.HasIndex(c => c.Mail).IsUnique();
+
+                entity.HasCheckConstraint("ck_mail_not_null", "[utl_mail] not null");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
